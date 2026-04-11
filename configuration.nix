135458192fe3e6ee2aea22d379b8eb56d881f1ad
phnsh <phn@sh.com>
2026-04-11 22:52:13 +0530

    big fix

--- configuration.nix
+++ configuration.nix
@@ -7,7 +7,7 @@
 {
   imports =
     [ # Include the results of the hardware scan.
-      ./hardware-configuration.nix
+      /home/punx/git-stuff/nixos-config/hardware-configuration.nix
       <home-manager/nixos> # home manager
     ];
 
